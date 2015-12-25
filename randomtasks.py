816170f0221c9de6b7cb5b70816aa1ea6d076ed0
Alexander Joulego <alexjoul@gmail.com>
2015-12-25 04:10:30 +0300

    add randomtasks_bisect; remove debugging output

--- randomtasks.py
+++ randomtasks.py
@@ -1,10 +1,15 @@
-import random
+import random, itertools, bisect
 
 def randomtasks(tasks):
 	population = [val for val, cnt in tasks for i in range(cnt)]
-	print(population)
 	return random.choice(population)
+
+def randomtasks_bisect(tasks):
+	choices, weights = zip(*tasks)
+	cumdist = list(itertools.accumulate(weights))
+	x = random.random() * cumdist[-1]
+	return choices[bisect.bisect(cumdist, x)]
 
 if __name__ == '__main__':
 	tasks = [('CS', 8), ('Math', 4), ('Lang', 5), ('Sci', 2)]
-	print(randomtasks(tasks))
+	print(randomtasks_bisect(tasks))
